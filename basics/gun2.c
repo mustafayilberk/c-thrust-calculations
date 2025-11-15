d60dad0b19ee4f75c554c36496852de6298b1ee5
mustafayilberk <mustafayilberk@gmail.com>
2025-11-15 14:28:19 +0300

    Add files via upload

---
+++ basics/gun2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int sayi = 10;
+    printf("Sayi: %d\n", sayi);
+    return 0;
+}
---
+++ basics/gun2_scanf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int yas;
+
+    printf("Yasinizi girin:");
+    scanf("%d",&yas);
+
+    printf("Girdiginiz yas:%d\n",yas);
+    return 0;
+
+}    
